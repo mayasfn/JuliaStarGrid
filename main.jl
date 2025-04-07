ed799d639ca5170c6698a3928acbc8d40ed343ea
Maya <maya.soufan03@gmail.com>
2025-04-07 22:53:57 +0200

    add: direcition constraints

--- main.jl
+++ main.jl
@@ -1,24 +1,42 @@
-using JuMP,HiGHS
-# using JuMP,Cbc
+#using JuMP,HiGHS
+using JuMP,Cbc
 
 function gridSolver(lignes::Array{Int64},colonnes::Array{Int64})
     n = size(lignes,1)
 
-    model = Model(HiGHS.Optimizer)
-    #model = Model(Cbc.Optimizer)
+    #model = Model(HiGHS.Optimizer)
+    model = Model(Cbc.Optimizer)
 
-    @variable(model,x[1:n,1:n],Bin)
+    @variable(model,x[1:n,1:n],Bin) #grille
+    
+    @variable(model,y[1:n,1:n,3,3],Bin) #direction 
 
     @objective(model,Max,0)
     # pttr des variable pour les directions
     @constraint(model,verticale[i in 1:n],sum(x[i,k] for k in 1:n) == lignes[i]) #pour les lignes
     @constraint(model,horizontale[j in 1:n],sum(x[k,j] for k in 1:n) == colonnes[j]) #pour les colonnes
     
+    # three stars per cell for y[i,j][starscells] where x[i,j]==1
+    @constraint(model, [i in 1:n, j in 1:n], sum(y[i,j,k,l] for k in 1:3, l in 1:3) == 3 * x[i,j])
+
+    # if x[i,j] == 1 and x[i+1,j]==1 then in y[i,j,2,3]==1 -> top
+    @constraint(model, [i in 1:n-1, j in 1:n], y[i,j,2,3] >= x[i,j] + x[i+1,j] - 1)
+
+    # if x[i,j]==1 then y[i,j,2,2]==1 -> center cell
+    @constraint(model, [i in 1:n, j in 1:n], y[i,j,2,2] == x[i,j])
+
+    # if x[i,j] == 1 and x[i-1,j]==1 then in y[i,j,2,1]==1 -> bottom
+    @constraint(model, [i in 2:n, j in 1:n], y[i,j,2,1] >= x[i,j] + x[i-1,j] - 1)
+
+    # if x[i,j] == 1 and x[i,j+1]==1 then in y[i,j,1,2]==1 -> left cell
+    @constraint(model, [i in 1:n, j in 2:n], y[i,j,3,2] >= x[i,j] + x[i,j-1] - 1)
+
+    # if x[i,j] == 1 and x[i,j-1]==1 then in y[i,j,3,2]==1 -> right cell
+    @constraint(model, [i in 1:n, j in 1:n-1], y[i,j,1,2] >= x[i,j] + x[i,j+1] - 1)
 
     # pour les directions
     # faut pas que l'entree et la sortie soient le smemes
     # faut pas avoir une sortie en diagonale
-    # les entrees ou sorties sont : Up,Down,Left,Right
 
     optimize!(model)
 
